262b35cf6967acfaa85d13f0cd7bfd5aa4350365
Sergey Grigorev <xors.nn@gmail.com>
2016-08-01 11:41:30 +0300

    add more info for replacing release

--- index.js
+++ index.js
@@ -179,7 +179,10 @@ module.exports = function (packageFile, opt) {
 				if (res.statusCode >= 400) {
 					// if replace flag is set, then we delete version to re-create it
 					if (replace) {
-						deleteVersion(version);
+						console.log("version exists replacing");
+						var delRes = deleteVersion(version);
+						console.log(delRes);
+						console.log("version deleted, now creating");
 						createVersion(version, false);
 					} else {
 						throw new PluginError("gulp-sentry-release.createVersion(version)", "Version existed. " + body);
